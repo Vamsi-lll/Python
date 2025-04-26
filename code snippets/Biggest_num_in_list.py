a357772aec0a00de7c019517202f46d446e9d9a0
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2025-04-27 01:31:45 +0530

    Update Biggest_num_in_list.py

--- code snippets/Biggest_num_in_list.py
+++ code snippets/Biggest_num_in_list.py
@@ -1,4 +1,17 @@
-#approch-1
-# by using predefined modules
-lst=[2,4,5,7,1,6,8]
+# approch-1
+# using predefined modules
+
+lst=[2,4,5,7,1,6,8,333,23]
 print(f"biggest num  in the given list list {max(lst)}")
+
+
+# approch-2
+#uing own sorting and finding the biggest
+
+big=0
+new_list=[]
+for i in lst:
+    if i>big:
+        big=i
+        new_list.append(big)
+print(f"biggest in the given list is {new_list[-1]}")
