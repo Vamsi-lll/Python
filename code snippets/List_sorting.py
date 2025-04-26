591298db46be2c11400708eb61cfface19c8b050
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2025-04-27 01:43:22 +0530

    Update List_sorting.py

--- code snippets/List_sorting.py
+++ code snippets/List_sorting.py
@@ -1,1 +1,11 @@
+lst = [2, 4, 5, 7, 1, 6, 8, 333, 23]
+print(f"before sorting = {lst}")
+
+n = len(lst)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if lst[j] > lst[j+1]:
+            lst[j], lst[j+1] = lst[j+1], lst[j]  # Swap elements
+
+print(f"after sorting = {lst}")
 
