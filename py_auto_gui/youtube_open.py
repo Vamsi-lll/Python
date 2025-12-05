b92001c42a130a821e57a3d06bf05497fe7ca89b
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2025-12-05 18:17:08 +0530

    Add comments to youtube_open.py for clarity
    Added comments to clarify the purpose of the script and its steps.

--- py_auto_gui/youtube_open.py
+++ py_auto_gui/youtube_open.py
@@ -1,8 +1,10 @@
+# import the required modules
 import pyautogui as auto
 import time
 
+# script to open the youtube in the chrome
 auto.press("win")
-time.sleep(2)
+time.sleep(2) # to delay the process for the system to responce
 auto.typewrite("chrome",interval=0.5)
 auto.press("enter")
 time.sleep(2)
@@ -10,7 +12,7 @@ auto.press("tab")
 time.sleep(2)
 auto.press("enter")
 time.sleep(2)
-auto.hotkey("ctrl","t")
+auto.hotkey("ctrl","t")  # special hot key for opening new tab in the chrome
 time.sleep(1)
 auto.typewrite("youtube.com",interval=0.5)
 auto.press("enter")
