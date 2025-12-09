537225139d70b4052be79091f4826285e88412f9
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2025-12-09 13:26:27 +0530

    Fix word counting logic and add new method

--- code snippets/word_counter.py
+++ code snippets/word_counter.py
@@ -9,3 +9,16 @@ with open("data.txt", "r") as file:
         word_count[word] = word_count.get(word, 0) + 1
 
 print(word_count)
+
+
+
+''' method -2'''
+'''
+sentence = "the quick brown fox jumps over the lazy dog and the quick blue hare"
+for word in sentence.split():
+    word = word.lower().strip(".,!?")
+    if word not in word_coun:
+        word_coun[word] = 1
+    else:
+        word_coun[word] += 1
+'''
