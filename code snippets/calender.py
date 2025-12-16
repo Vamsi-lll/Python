18b0e5282ca4860bbd7b61b83e25b603f64b2f91
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2025-12-16 17:51:13 +0530

    Add comments to calendar.py for clarity

--- code snippets/calender.py
+++ code snippets/calender.py
@@ -1,10 +1,10 @@
 # calender print using the calender module
-import datetime
-import calendar
+import datetime  #for getting the current tyme and date from the device
+import calendar  #for printing the calender
 
-today = datetime.date.today()
+today = datetime.date.today()   #creating the object for the datatime class
 
-year = today.year
-month = today.month
+year = today.year  #for getting the current year
+month = today.month #for getting the current month
 
-print(calendar.month(year, month))
+print(calendar.month(year, month))  #for printing the calender 
