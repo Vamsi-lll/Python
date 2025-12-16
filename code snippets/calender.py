bc1649d9ae50b83d455a6f816bdbb0a28622f4f3
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2025-12-16 17:48:39 +0530

    Add calendar printing functionality using datetime

--- code snippets/calender.py
+++ code snippets/calender.py
@@ -1,1 +1,10 @@
 # calender print using the calender module
+import datetime
+import calendar
+
+today = datetime.date.today()
+
+year = today.year
+month = today.month
+
+print(calendar.month(year, month))
