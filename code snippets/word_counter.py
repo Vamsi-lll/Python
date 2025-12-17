9fe94b96a243eff3e7eeef362f9e2bec6b4f2fd5
Krishna Vamsi . M <Vamsi.maddipati2@gmail.com>
2025-12-17 11:22:27 +0530

    Refactor word counter logic in word_counter.py

--- code snippets/word_counter.py
+++ code snippets/word_counter.py
@@ -1,16 +1,11 @@
 word_count = {}
-
 with open("data.txt", "r") as file:
     text = file.read()
     words = text.split()   
-
     for word in words:
         # increment count if word exists, else start at 1
         word_count[word] = word_count.get(word, 0) + 1
-
 print(word_count)
-
-
 
 ''' method -2'''
 '''
@@ -22,7 +17,6 @@ for word in sentence.split():
     else:
         word_coun[word] += 1
 '''
-
 
 '''----------------------------- task-----------------------------------'''
 ''' inp:aaabbcccdd
